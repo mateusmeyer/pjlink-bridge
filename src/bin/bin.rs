a83fc73c8948907a4ce46b7d99fca66c016f815c
Mateus Meyer Jiacomelli <mateus@sportizze.com>
2021-07-12 01:33:04 -0300

    base handler (wip); base mock implementation (wip)

--- src/bin/bin.rs
+++ src/bin/bin.rs
@@ -1,17 +1,11 @@
-use pjlink_bridge::{
-    PjLinkListener,
-    PjLinkHandler,
-    PjLinkCommand,
-    PjLinkRawPayload,
-    PjLinkResponse,
-    PjLinkPowerCommandParameter,
-};
+use pjlink_bridge::*;
 
 use std::net::TcpListener;
 
 pub fn main() {
     let handler = PjLinkMockProjector::new();
-    let tcp_listener = TcpListener::bind("127.0.0.1:4352").unwrap();
+    let tcp_listener = TcpListener::bind("0.0.0.0:4352").unwrap();
+    tcp_listener.set_nonblocking(false).unwrap();
     let mut listener: PjLinkListener = PjLinkListener::new(handler, tcp_listener);
     listener.listen();
 }
@@ -19,7 +13,16 @@ pub fn main() {
 
 #[derive(Clone)]
 struct PjLinkMockProjectorState{
-    power_on: PjLinkPowerCommandParameter
+    power_on: u8,
+    error_fan_status: u8,
+    error_lamp_status: u8,
+    error_temperature_status: u8,
+    error_cover_open_status: u8,
+    error_filter_status: u8,
+    error_other_status: u8,
+    lamp_hours: Vec<u8>,
+    mute_status: [u8; 2],
+    input_status: [u8; 2],
 }
 
 struct PjLinkMockProjector<'a> {
@@ -32,7 +35,16 @@ impl PjLinkMockProjector<'_> {
         return PjLinkMockProjector {
             password: Option::None,
             state: PjLinkMockProjectorState {
-                power_on: PjLinkPowerCommandParameter::Off
+                power_on: PjLinkPowerCommandStatus::Off,
+                error_fan_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                error_lamp_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                error_temperature_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                error_cover_open_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                error_filter_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                error_other_status: PjLinkErrorStatusCommandStatusItem::Normal,
+                lamp_hours: Vec::from([b'1', b'2', b'0']),
+                mute_status: [PjLinkMuteCommandStatus::AudioAndVideo, PjLinkMuteCommandStatus::NonMute],
+                input_status: [PjLinkInputCommandStatus::RGB, b'1'],
             }
         }
     }
@@ -40,21 +52,135 @@ impl PjLinkMockProjector<'_> {
 
 impl PjLinkHandler for PjLinkMockProjector<'_> {
 
-    fn handle_command(&mut self, command: PjLinkCommand, _raw_command: PjLinkRawPayload) -> PjLinkResponse {
+    fn handle_command(&mut self, command: PjLinkCommand, _raw_command: &PjLinkRawPayload) -> PjLinkResponse {
         return match command {
             PjLinkCommand::Power1(PjLinkPowerCommandParameter::On) => {
                 println!("Power On Projector");
-                self.state.power_on = PjLinkPowerCommandParameter::On;
-                return PjLinkResponse::OK;
+                self.state.power_on = PjLinkPowerCommandStatus::On;
+                return PjLinkResponse::Ok;
             }
             PjLinkCommand::Power1(PjLinkPowerCommandParameter::Off) => {
                 println!("Power Off Projector");
-                self.state.power_on = PjLinkPowerCommandParameter::Off;
-                return PjLinkResponse::OK;
+                self.state.power_on = PjLinkPowerCommandStatus::Off;
+                return PjLinkResponse::Ok;
             }
             PjLinkCommand::Power1(PjLinkPowerCommandParameter::Query) => {
                 println!("Query Power Status");
-                return PjLinkResponse::Response(Box::new(self.state.power_on))
+                return PjLinkResponse::OkSingle(self.state.power_on)
+            }
+            PjLinkCommand::Class1 => {
+                println!("Query Projector Class");
+                return PjLinkResponse::OkSingle(PjLinkClassCommandStatus::Class1)
+            }
+            PjLinkCommand::ErrorStatus1 => {
+                println!("Error Status Query");
+                return PjLinkResponse::OkMultiple(Vec::from([
+                    self.state.error_fan_status,
+                    self.state.error_lamp_status,
+                    self.state.error_temperature_status,
+                    self.state.error_cover_open_status,
+                    self.state.error_filter_status,
+                    self.state.error_other_status
+                ]));
+            }
+            PjLinkCommand::Lamp1 => {
+                println!("Lamp Query");
+                let mut hours = self.state.lamp_hours.clone();
+                hours.push(b' ');
+                hours.push(self.state.power_on);
+                return PjLinkResponse::OkMultiple(hours);
+            }
+            PjLinkCommand::InfoManufacturer1 => {
+                println!("Info Manufacturer Query");
+                return PjLinkResponse::OkMultiple(Vec::from("mateusmeyer mocks"));
+            }
+            PjLinkCommand::InfoProductName1 => {
+                println!("Info Product Name Query");
+                return PjLinkResponse::OkMultiple(Vec::from("pjlink-bridge-mock"));
+            }
+            PjLinkCommand::Name1 => {
+                println!("Name Query");
+                return PjLinkResponse::OkMultiple(Vec::from("projector-001"));
+            }
+            PjLinkCommand::InfoOther1 => {
+                println!("Info Other Query");
+                return PjLinkResponse::OkMultiple(Vec::from([]));
+            }
+            PjLinkCommand::Input1(PjLinkInputCommandParameter::Query) | PjLinkCommand::Input2(PjLinkInputCommandParameter::Query) => {
+                println!("Input1|2 Query");
+                return PjLinkResponse::OkMultiple(Vec::from(self.state.input_status));
+            },
+            PjLinkCommand::Input1(input) | PjLinkCommand::Input2(input) => {
+                println!("Input1|2 Set");
+
+                match input {
+                    PjLinkInputCommandParameter::RGB(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::RGB, value];
+                    }
+                    PjLinkInputCommandParameter::Video(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::Video, value];
+                    }
+                    PjLinkInputCommandParameter::Digital(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::Digital, value];
+                    }
+                    PjLinkInputCommandParameter::Storage(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::Storage, value];
+                    }
+                    PjLinkInputCommandParameter::Network(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::Network, value];
+                    }
+                    PjLinkInputCommandParameter::Internal(value) => {
+                        self.state.input_status = [PjLinkInputCommandStatus::Internal, value];
+                    }
+                    _ => return PjLinkResponse::OutOfParameter
+                };
+
+                return PjLinkResponse::Ok;
+            },
+            PjLinkCommand::AvMute1(PjLinkMuteCommandParameter::Query) => {
+                println!("AV Mute Query");
+                return PjLinkResponse::OkMultiple(Vec::from(self.state.mute_status))
+            }
+            PjLinkCommand::AvMute1(parameter) => {
+                println!("AV Mute Set");
+                let is_muted = self.state.mute_status[1] == PjLinkMuteCommandStatus::Mute;
+                let current_muted_item = self.state.mute_status[0];
+
+                match parameter {
+                    PjLinkMuteCommandParameter::Audio(mute) => {
+                        self.state.mute_status = if current_muted_item == PjLinkMuteCommandStatus::Video && is_muted && mute {
+                            [PjLinkMuteCommandStatus::AudioAndVideo, PjLinkMuteCommandStatus::Mute]
+                        } else {
+                            if current_muted_item == PjLinkMuteCommandStatus::AudioAndVideo && is_muted && !mute {
+                                [PjLinkMuteCommandStatus::Video, PjLinkMuteCommandStatus::Mute]
+                            } else {
+                                [current_muted_item, if mute {PjLinkMuteCommandStatus::Mute} else {PjLinkMuteCommandStatus::NonMute}]
+                            }
+                        }
+                    }
+                    PjLinkMuteCommandParameter::Video(mute) => {
+                        self.state.mute_status = if current_muted_item == PjLinkMuteCommandStatus::Audio && is_muted && mute {
+                            [PjLinkMuteCommandStatus::AudioAndVideo, PjLinkMuteCommandStatus::Mute]
+                        } else {
+                            if current_muted_item == PjLinkMuteCommandStatus::AudioAndVideo && is_muted && !mute {
+                                [PjLinkMuteCommandStatus::Audio, PjLinkMuteCommandStatus::Mute]
+                            } else {
+                                [current_muted_item, if mute {PjLinkMuteCommandStatus::Mute} else {PjLinkMuteCommandStatus::NonMute}]
+                            }
+                        }
+                    }
+                    PjLinkMuteCommandParameter::AudioAndVideo(mute) => {
+                        self.state.mute_status = [
+                            PjLinkMuteCommandStatus::AudioAndVideo,
+                            if mute {PjLinkMuteCommandStatus::Mute} else {PjLinkMuteCommandStatus::NonMute}
+                        ];
+                    },
+                    _ => {
+                        return PjLinkResponse::OutOfParameter;
+                    }
+                }
+
+                return PjLinkResponse::Ok;
             }
             _ => PjLinkResponse::OutOfParameter
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 extern crate rand;
 
-use std::any::Any;
 use std::thread;
 use std::sync::{RwLock, Arc};
 use std::net::{TcpListener, TcpStream};
-use std::convert::TryFrom;
 use std::io::{Read, Write};
 use rand::prelude::*;
 
@@ -12,11 +10,12 @@ pub fn teste() {
     println!("Teste")
 }
 
-pub const PJLINK_HEADER_CHAR: u8 = '%' as u8;
+pub const PJLINK_HEADER: u8 = b'%';
 pub const PJLINK_COMMAND_SEPARATOR: u8 = 0x20; // space
 pub const PJLINK_RESPONSE_SEPARATOR: u8 = 0x3d; // =
-pub const PJLINK_TERMINATOR: u8 = 0x0d; // carriage return
-pub const PJLINK_QUERY: u8 = '?' as u8;
+pub const PJLINK_TERMINATOR: u8 = b'\x0d'; // carriage return
+pub const PJLINK_QUERY_CHAR: char = '?';
+pub const PJLINK_QUERY: u8 = PJLINK_QUERY_CHAR as u8;
 
 pub const PJLINK_NULLIFIED_SECURITY: &[u8; 9] = b"PJLINK 0\x0d";
 pub const PJLINK_SECURITY: &[u8; 9] = b"PJLINK 1 ";
@@ -36,29 +35,99 @@ pub struct PjLinkRawNoBodyPayload {
 }
 
 pub enum PjLinkResponse {
-    OK,
+    Ok,
     Undefined,
     OutOfParameter,
     UnavailableTime,
     ProjectorOrDisplayFailure,
-    Response(Box<dyn Any>)
+    OkSingle(u8),
+    OkMultiple(Vec<u8>)
+}
+
+pub enum PjLinkPowerCommandParameter {
+    Off,
+    On,
+    Query,
+    Unknown,
+}
+
+pub struct PjLinkPowerCommandStatus;
+#[allow(non_upper_case_globals)]
+impl PjLinkPowerCommandStatus {
+    pub const Off: u8 = b'0';
+    pub const On: u8 = b'1';
+    pub const Cooling: u8 = b'2';
+    pub const WarmUp: u8 = b'3';
+}
+
+pub struct PjLinkClassCommandStatus;
+#[allow(non_upper_case_globals)]
+impl PjLinkClassCommandStatus {
+    pub const Class1: u8 = b'1';
+    pub const Class2: u8 = b'2';
+}
+
+pub struct PjLinkErrorStatusCommandStatusItem;
+#[allow(non_upper_case_globals)]
+impl PjLinkErrorStatusCommandStatusItem {
+    pub const Normal: u8 = b'0';
+    pub const Warning: u8 = b'1';
+    pub const Error: u8 = b'2';
+}
+
+pub struct PjLinkInputCommandStatus;
+#[allow(non_upper_case_globals)]
+impl PjLinkInputCommandStatus {
+    pub const RGB: u8 = b'1';
+    pub const Video: u8 = b'2';
+    pub const Digital: u8 = b'3';
+    pub const Storage: u8 = b'4';
+    pub const Network: u8 = b'5';
+    pub const Internal: u8 = b'6';
+}
+pub enum PjLinkInputCommandParameter {
+    RGB(u8),
+    Video(u8),
+    Digital(u8),
+    Storage(u8),
+    Network(u8),
+    Internal(u8),
+    Query,
+    Unknown,
+}
+
+pub struct PjLinkMuteCommandStatus;
+#[allow(non_upper_case_globals)]
+impl PjLinkMuteCommandStatus {
+    pub const Audio: u8 = b'2';
+    pub const Video: u8 = b'1';
+    pub const AudioAndVideo: u8 = b'3';
+    pub const Mute: u8 = b'1';
+    pub const NonMute: u8 = b'0';
+}
+pub enum PjLinkMuteCommandParameter {
+    Audio(bool),
+    Video(bool),
+    AudioAndVideo(bool),
+    Query,
+    Unknown,
 }
 
 pub enum PjLinkCommand {
     Search2,
     Power1(PjLinkPowerCommandParameter),
-    Input1(PjLinkInputType, u8),
-    Input2(PjLinkInputType, u8),
-    AvMute1(PjLinkMuteType, u8),
-    ErrorStatus1(u8),
-    Lamp1(u8),
+    Input1(PjLinkInputCommandParameter),
+    Input2(PjLinkInputCommandParameter),
+    AvMute1(PjLinkMuteCommandParameter),
+    ErrorStatus1,
+    Lamp1,
     InputTogglingList1(u8),
     InputTogglingList2(u8),
-    Name1(u8),
-    InfoManufacturer1(u8),
-    InfoProductName1(u8),
-    InfoOther1(u8),
-    Class1(u8),
+    Name1,
+    InfoManufacturer1,
+    InfoProductName1,
+    InfoOther1,
+    Class1,
     SerialNumber2(u8),
     SoftwareVersion2(u8),
     InputTerminalName2(u8),
@@ -81,41 +150,10 @@ pub enum PjLinkStatusCommand {
     Input2(u8, u8),
 }
 
-#[derive(Clone, Copy)]
-pub enum PjLinkPowerCommandParameter {
-    Off,
-    On,
-    Query,
-    Unknown,
-}
-
-pub enum PjLinkPowerCommandStatus {
-    Off,
-    On,
-    Cooling,
-    WarmUp,
-}
-
-pub enum PjLinkInputType {
-    RGB(u8),
-    Video(u8),
-    Digital(u8),
-    Storage(u8),
-    Network(u8),
-    Internal(u8),
-    Query,
-}
-
-pub enum PjLinkMuteType {
-    Audio(bool),
-    Video(bool),
-    AudioAndVideo(bool),
-    Query,
-}
 
 pub trait PjLinkHandler: Sync + Send  {
     fn get_password(&mut self) -> Option<&String>;
-    fn handle_command(&mut self, command: PjLinkCommand, raw_command: PjLinkRawPayload) -> PjLinkResponse;
+    fn handle_command(&mut self, command: PjLinkCommand, raw_command: &PjLinkRawPayload) -> PjLinkResponse;
 }
 
 pub struct PjLinkListener<'a> {
@@ -158,40 +196,78 @@ struct PjLinkConnectionHandler {
 
 impl PjLinkConnectionHandler{
     fn handle_connection(&mut self, mut stream: TcpStream) {
-        let mut buffer = [0u8; 256];
         let lock_handler = &self.handler;
 
         if let Ok(mut handler) = lock_handler.write() {
+            let mut auth_buffer = Vec::<u8>::new();
             let password: Option<&String> = handler.get_password();
 
             if password.is_none() {
-                Self::generate_nullified_security(&mut buffer);
+                Self::generate_nullified_security(&mut auth_buffer);
             } else {
                 let number = Self::generate_random_number();
-                Self::generate_password_security(&mut buffer, number);
+                Self::generate_password_security(&mut auth_buffer, number);
             }
 
-            stream.write(&buffer).unwrap();
+            stream.write(&auth_buffer).unwrap();
             stream.flush().unwrap();
+        }
 
-            let mut input_command_buffer = [0u8; 256];
-            stream.read(&mut input_command_buffer).unwrap();
+        'message: loop {
+            let mut input_command_buffer = Vec::<u8>::new();
+            'input: loop {
+                let mut char_buffer = [0u8; 1];
+                match stream.read_exact(&mut char_buffer) {
+                    Ok(_) => {
+                        if char_buffer[0] == PJLINK_TERMINATOR {
+                            break 'input;
+                        } else {
+                            input_command_buffer.extend(char_buffer);
+                        }
+                    }
+                    Err(_) => {
+                        break 'message;
+                    }
+                }
+            }
 
             let raw_command = self.to_raw_command(input_command_buffer);
+            print!("Command:");
+
+            for byte in raw_command.command_body {
+                print!("{}", byte as char)
+            }
+            println!("");
+            
             let command = self.get_command(&raw_command);
 
-            handler.handle_command(command, raw_command);
+            if let Ok(mut handler) = lock_handler.write() {
+                let response = handler.handle_command(command, &raw_command);
+                let raw_response = self.to_raw_response(raw_command, response);
+                let output_buffer = self.write_to_buffer(raw_response);
+                match stream.write(&output_buffer) {
+                    Ok(_) => {
+                        match stream.flush() {
+                            Ok(_) => continue 'message,
+                            Err(_) => break 'message
+                        }
+                    }
+                    Err(_) => break 'message
+                }
+            }
         }
     }
 
     fn get_command(&self, raw_command: &PjLinkRawPayload) -> PjLinkCommand {
         let transmission_parameter = &raw_command.transmission_parameter;
-        let class = [raw_command.header_and_class[1]];
-        let mut command_body_string = std::str::from_utf8(&class).unwrap().to_owned();
+        let class = raw_command.header_and_class[1];
+        let mut command_body_string = std::str::from_utf8(&[class]).unwrap().to_owned();
         command_body_string.push_str(
             std::str::from_utf8(&raw_command.command_body).unwrap()
         );
         let command_body_str = command_body_string.as_str();
+        let is_class_2 = class == b'2';
+        let transmission_parameter_len = transmission_parameter.len();
 
         return match command_body_str {
             "1POWR" => {
@@ -199,20 +275,97 @@ impl PjLinkConnectionHandler{
                 let parameter = match raw_parameter as char {
                     '1' => PjLinkPowerCommandParameter::On,
                     '0' => PjLinkPowerCommandParameter::Off,
-                    '?' => PjLinkPowerCommandParameter::Query,
+                    PJLINK_QUERY_CHAR => PjLinkPowerCommandParameter::Query,
                     _ => PjLinkPowerCommandParameter::Unknown, 
                 };
 
                 return PjLinkCommand::Power1(parameter);
+            },
+            "1CLSS" => PjLinkCommand::Class1,
+            "1ERST" => PjLinkCommand::ErrorStatus1,
+            "1LAMP" => PjLinkCommand::Lamp1,
+            "1INFO" => PjLinkCommand::InfoOther1,
+            "1INF1" => PjLinkCommand::InfoManufacturer1,
+            "1INF2" => PjLinkCommand::InfoProductName1,
+            "1NAME" => PjLinkCommand::Name1,
+            "1AVMT" => {
+                let parameter = if transmission_parameter_len == 1 && transmission_parameter[0] == PJLINK_QUERY {
+                    PjLinkMuteCommandParameter::Query
+                } else if transmission_parameter_len == 2 {
+                    let raw_parameter = (transmission_parameter[0], transmission_parameter[1]);
+                    match raw_parameter {
+                        (b'1', b'1') => PjLinkMuteCommandParameter::Video(true),
+                        (b'1', b'0') => PjLinkMuteCommandParameter::Video(false),
+                        (b'2', b'1') => PjLinkMuteCommandParameter::Audio(true),
+                        (b'2', b'0') => PjLinkMuteCommandParameter::Audio(false),
+                        (b'3', b'1') => PjLinkMuteCommandParameter::AudioAndVideo(true),
+                        (b'3', b'0') => PjLinkMuteCommandParameter::AudioAndVideo(false),
+                        _ => PjLinkMuteCommandParameter::Unknown
+                    }
+                } else {
+                    PjLinkMuteCommandParameter::Unknown
+                };
+
+                return PjLinkCommand::AvMute1(parameter);
+            }
+            "1INPT" | "2INPT" => {
+                let parameter = if transmission_parameter_len == 1 && transmission_parameter[0] == PJLINK_QUERY {
+                    PjLinkInputCommandParameter::Query
+                } else if transmission_parameter_len == 2 {
+                    let (input_char, input_value) = (transmission_parameter[0], transmission_parameter[1]);
+                    match input_char {
+                        b'1' => PjLinkInputCommandParameter::RGB(input_value),
+                        b'2' => PjLinkInputCommandParameter::Video(input_value),
+                        b'3' => PjLinkInputCommandParameter::Digital(input_value),
+                        b'4' => PjLinkInputCommandParameter::Network(input_value),
+                        b'5' => if is_class_2 {
+                            PjLinkInputCommandParameter::Internal(input_value)
+                        } else {
+                            PjLinkInputCommandParameter::Unknown
+                        }
+                        _ => PjLinkInputCommandParameter::Unknown
+                    }
+                } else {
+                    PjLinkInputCommandParameter::Unknown
+                };
+
+                return if is_class_2 {
+                    PjLinkCommand::Input2(parameter)
+                } else {
+                    PjLinkCommand::Input1(parameter)
+                }
             }
             _ => PjLinkCommand::Unknown
         }
     }
 
-    fn to_raw_command(&self, command: [u8; 256]) -> PjLinkRawPayload {
+    fn to_raw_response(&self, raw_command: PjLinkRawPayload, response: PjLinkResponse) -> PjLinkRawPayload {
+        let header_and_class: [u8; 2] = raw_command.header_and_class;
+        let command_body: [u8; 4] = raw_command.command_body;
+        let separator: u8 = PJLINK_RESPONSE_SEPARATOR;
+        let transmission_parameter: Vec<u8> = match response {
+            PjLinkResponse::Ok => Vec::from("OK"),
+            PjLinkResponse::OutOfParameter => Vec::from("ERR2"),
+            PjLinkResponse::UnavailableTime => Vec::from("ERR3"),
+            PjLinkResponse::ProjectorOrDisplayFailure => Vec::from("ERR4"),
+            PjLinkResponse::Undefined => Vec::from("ERR1"),
+            PjLinkResponse::OkSingle(response_value) => Vec::from([response_value]),
+            PjLinkResponse::OkMultiple(response_value) => Vec::from(response_value),
+        };
+
+        return PjLinkRawPayload {
+            header_and_class,
+            command_body,
+            separator,
+            transmission_parameter,
+            terminator: PJLINK_TERMINATOR
+        };
+    }
+
+    fn to_raw_command(&self, command: Vec<u8>) -> PjLinkRawPayload {
         let mut header_and_class: [u8; 2] = Default::default();
         let mut command_body: [u8; 4] = Default::default();
-        let transmission_parameter: Vec<u8> = command[7..256].to_vec();
+        let transmission_parameter: Vec<u8> = command[7..command.len()].to_vec();
 
         header_and_class.copy_from_slice(&command[0..2]);
         command_body.copy_from_slice(&command[2..6]);
@@ -228,17 +381,35 @@ impl PjLinkConnectionHandler{
         return command;
     }
 
+    fn write_to_buffer(&self, mut raw_response: PjLinkRawPayload) -> Vec<u8> {
+        let mut buffer = Vec::<u8>::new();
+        buffer.extend(&raw_response.header_and_class);
+        buffer.extend(&raw_response.command_body);
+        buffer.push(raw_response.separator);
+
+        buffer.append(&mut raw_response.transmission_parameter);
+        let buffer_last = buffer.len() - 1;
+
+        if buffer[buffer_last] == b'\x00' {
+            buffer[buffer_last] = PJLINK_TERMINATOR;
+        } else {
+            buffer.push(PJLINK_TERMINATOR);
+        }
+
+        return buffer;
+    }
+
     fn generate_random_number() -> u32 {
         let mut rng = rand::thread_rng();
         return rng.next_u32()
     }
 
-    fn generate_nullified_security(buffer: &mut [u8; 256]) {
-        buffer[0..9].copy_from_slice(PJLINK_NULLIFIED_SECURITY);
+    fn generate_nullified_security(buffer: &mut Vec<u8>) {
+        buffer.extend(PJLINK_NULLIFIED_SECURITY);
     }
 
-    fn generate_password_security(buffer: &mut [u8; 256], number: u32) {
-        buffer[0..9].copy_from_slice(PJLINK_SECURITY);
-        buffer[10..17].copy_from_slice(format!("{:08X}", number).as_bytes());
+    fn generate_password_security(buffer: &mut Vec<u8>, number: u32) {
+        buffer.extend(PJLINK_SECURITY);
+        buffer.extend(format!("{:08X}", number).as_bytes());
     }
 }
